793dc8caa77406d17117921a58224d01b0995c75
brice-syslogic <65510350+brice-syslogic@users.noreply.github.com>
2022-12-16 11:32:52 -0800

    Adding cyberspect_completed_scans

--- mobsf/MobSF/urls.py
+++ mobsf/MobSF/urls.py
@@ -58,8 +58,10 @@ urlpatterns = [
     re_path(r'^api/v1/delete_suppression$', api_sz.api_delete_suppression),
     re_path(r'^api/v1/cyberspect_scan$', api_sz.api_cyberspect_get_scan),
     re_path(r'^api/v1/cyberspect_scans$', api_sz.api_cyberspect_recent_scans),
+    re_path(r'^api/v1/cyberspect_completedscans$',
+            api_sz.api_cyberspect_completed_scans),
     re_path(r'^api/v1/update_cyberspect_scan$',
-            api_sz.api_update_cyberspect_scan),
+            api_sz.api_update_cyberspect_scan),    
     # Dynamic Analysis
     re_path(r'^api/v1/dynamic/get_apps$', api_dz.api_get_apps),
     re_path(r'^api/v1/dynamic/start_analysis$', api_dz.api_start_analysis),
