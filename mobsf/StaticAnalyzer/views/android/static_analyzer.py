ef200cd108eb4ca5ceac347b8f0dab1c11650f05
brice-syslogic <65510350+brice-syslogic@users.noreply.github.com>
2024-09-23 20:09:32 -0700

    Lint fixes #4

--- mobsf/StaticAnalyzer/views/android/static_analyzer.py
+++ mobsf/StaticAnalyzer/views/android/static_analyzer.py
@@ -258,7 +258,7 @@ def static_analyzer(request_data, checksum, api=False):
                 tracker_res = tracker.get_trackers()
 
                 apk_2_java(app_dic['app_path'],
-                           app_dic['app_dir'], 
+                           app_dic['app_dir'],
                            app_dic['tools_dir'])
 
                 dex_2_smali(app_dic['app_dir'], app_dic['tools_dir'])
