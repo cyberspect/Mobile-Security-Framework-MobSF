f61df42d48dc589bf97e19d71e819cf6ad77bded
Chris Rickard <crickard@syslogicinc.com>
2025-10-21 15:57:48 -0400

    This quiets a false alarm for an upstream MobSF global var
    F824: global KNOWN_MIPMAP_SIZES` is unused: name is never assigned in scope

--- mobsf/StaticAnalyzer/views/android/icon_analysis.py
+++ mobsf/StaticAnalyzer/views/android/icon_analysis.py
@@ -106,7 +106,7 @@ def find_icon_path_zip(checksum, res_dir, icon_paths_from_manifest):
     fetched from the manifest and by global search
     returns an empty string on fail or a full path
     """
-    global KNOWN_MIPMAP_SIZES
+    global KNOWN_MIPMAP_SIZES  # noqa: F824
     try:
         msg = 'Guessing icon path'
         logger.info(msg)
