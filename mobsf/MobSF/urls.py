83fe0043d15176acdfa37b6f4128d6a1d4e3700e
Chris Rickard <crickard@syslogicinc.com>
2025-11-12 16:20:27 -0500

    Correct capitalization of MOBSF_HOME variable

--- mobsf/MobSF/urls.py
+++ mobsf/MobSF/urls.py
@@ -425,6 +425,6 @@ if settings.API_ONLY == '0':
 
 urllib3.disable_warnings()
 utils.print_version()
-install_jadx(settings.MobSF_HOME)
+install_jadx(settings.MOBSF_HOME)
 init_exec_hooks()
 store_exec_hashes_at_first_run()
