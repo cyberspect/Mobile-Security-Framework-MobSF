bd127792ea491f4cafdddb339840652d2babdc87
Chris Rickard <crickard@syslogicinc.com>
2026-01-16 10:14:00 -0500

    Restore MobSF code to reset settings.ASYNC_ANALYSIS = True

--- mobsf/StaticAnalyzer/views/android/apk.py
+++ mobsf/StaticAnalyzer/views/android/apk.py
@@ -155,9 +155,7 @@ def apk_analysis_task(checksum, app_dic, rescan, queue=False):
         update_cyberspect_scan({'id': cyberspect_scan_id, 'sast_start': utcnow()})
     try:
         if queue:
-            # Cyberspect mod - comment out: settings.ASYNC_ANALYSIS = True
-            # it's a code smell to change the Django global setting
-            # at runtime
+            settings.ASYNC_ANALYSIS = True
             mark_task_started(checksum)
         append_scan_status(checksum, 'init')
         get_size_and_hashes(app_dic)
@@ -310,9 +308,7 @@ def src_analysis_task(checksum, app_dic, rescan, pro_type, queue=False):
     context = None
     try:
         if queue:
-            # Cyberspect mod - comment out: settings.ASYNC_ANALYSIS = True
-            # it's a code smell to change the Django global setting
-            # at runtime
+            settings.ASYNC_ANALYSIS = True
             mark_task_started(checksum)
         cert_dic = {
             'certificate_info': '',
