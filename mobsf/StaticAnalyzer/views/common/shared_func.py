583557d8cc6daa63aa8139d43e9d4fe8e65c76b1
Chris Rickard <crickard@syslogicinc.com>
2026-08-21 15:30:10 -0400

    Fix lint 'line too long' error

--- mobsf/StaticAnalyzer/views/common/shared_func.py
+++ mobsf/StaticAnalyzer/views/common/shared_func.py
@@ -119,10 +119,12 @@ def unzip(checksum, app_path, ext_path):
     append_scan_status(checksum, msg)
     files = []
     original_ext_path = ext_path
-    # Cyberspect mod begins - fixes issue handling corrupt files
-    # Moved the stop_fallback_extraction flag outside the try block to ensure it's always defined
+    # Cyberspect mod begins
+    # Fixes issue handling corrupt files by moving the
+    # stop_fallback_extraction flag outside the try block to ensure it's
+    # always defined.
     stop_fallback_extraction = False
-    # Cyberspect mod ends 
+    # Cyberspect mod ends
     try:
         with zipfile.ZipFile(app_path, 'r') as zipptr:
             files = zipptr.namelist()
